4d0cf01f7f56f47b710c199cb90380b95102f5e2
Greg Ojasalu <greg.suur@gmail.com>
2025-09-30 09:48:18 +0300

    matemaatika näited

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-console.log('Hello world!');
-
-// muutujad
-var num = 1; // ära kasuta var'i enam
-let num2 = 2; 
-
-console.log(num2, num);
-num = 3.5;
-console.log(typeof num);
-let text = "ga";
-console.log(typeof text);
-let bool = true;
-console.log(typeof bool);
-bool = false;
-console.log(typeof bool);
-let nothing;
-console.log(typeof nothing);
-nothing = undefined;
-nothing = null;
-let array = [1, 2, 3, 4, "sad", true, 3.5];
-console.log(typeof array);
-let obj = {
-    name: "John",
-    age: 523,
-    cats: ['Koer', 'Kass', 'Jänes'],
-    data: {
-        phone: '3',
-        email: 'john@gmail.com'
-    }
-}
-console.log(typeof obj);
-
-function hello() {
-    console.log('hello');
-}
-
-console.log(typeof hello);
+let num = 1;
+console.log(num);
+num = num + 2;
+console.log(num);
+num = num - 1;
+console.log(num);
+num = num * 2;
+console.log(num);
+num = num / 3;
+console.log(num);
+num = 3;
+num = num ** 3;
+console.log(num);
+num = 9;
+num = num % 2; // modulo
+console.log(num);
+num += 2;
+num -= 2;
+num *= 2;
+num /= 2;
+num **= 2;
+num %= 2;
+console.log(num);
+num++; // lisab ühe
+console.log(num);
+num--; // lahutab ühe
+console.log(num);
+num = Math.sqrt(9);
+console.log(num);
