47d79246ee75162bb19a71354734480e80a1e70d
Greg Ojasalu <greg.suur@gmail.com>
2025-10-14 09:41:38 +0300

    loopivad käsud

--- script.js
+++ script.js
@@ -1,52 +1,33 @@
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+for(let i=9;i>=0;i--) {
+    console.log(i);
+}
+
+for(let i=1;i<1_000_000;i*=2) {
+    console.log(i);
+}
+
+for(let i=0;i>10;i++){
+    console.log(i);
+}
+
+let start = Date.now();
+console.log(start);
+let i = 0;
+while(Date.now() < start+5000) {
+    i++;
+}
+console.log(i);
+
 let num = 5;
-if(num > 10) {
-    console.log('suurem kui 10')
-} else if (num == 10){
-    console.log('võrdne')
-} else {
-    console.log('väiksem kui 10')
+
+while(num>5) {
+    console.log('while');
 }
 
-let day = new Date('2025-10-14').getDay();
-console.log(day);
-if(day === 1) {
-    console.log('esmaspäev');
-} else if (day === 2) {
-  console.log('teisipäev');
-} else if (day === 3) { 
-    console.log('kolmapäev');
-} else if (day === 4) { 
-    console.log('neljapäev');
-} else if (day === 5) {
-    console.log('reede');
-} else if (day === 6) {
-    console.log('laupäev');
-} else if (day === 0) {
-    console.log('pühapäev');
-}
-
-switch (day) {
-    case 1:
-        console.log('esmaspäev');
-        break;
-    case 2:
-        console.log('teisipäev');
-        break;
-    case 3:
-        console.log('kolmapäev');
-        break;
-    case 4:
-        console.log('neljapäev');
-        break;
-    case 5:
-        console.log('reede');
-        break;
-    case 6:
-        console.log('laupäev');
-        break;
-    case 0:
-        console.log('pühapäev');
-        break;
-    default: 
-        console.log('päev');
-}
+do {
+    console.log('do')
+} while (num>5);
