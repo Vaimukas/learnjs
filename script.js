0645612901c943459acc08a262e7d53ef6298682
Greg Ojasalu <greg.suur@gmail.com>
2025-10-28 09:12:58 +0200

    timeout ja interval

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let nums = [4,7,1,3,9,5,2,6,8];
+setTimeout(()=> {
+    console.log('üllatus');
+}, 10_000);
 
-nums.forEach(num => console.log(num));
-nums.forEach((num, i) => console.log(num, i));
+console.log('ühtegi üllatust ei tule');
 
-let even = nums.filter(num => num%2 === 0)
-console.log(even);
-let sum = nums.reduce((sum, num) => sum+num);
-console.log(sum);
+setInterval(() => {
+    console.log('mimi');
+}, 10_000 );
 
-let sorted = nums.sort((a,b) => {
-    if(a > b) {
-        return 1;
-    }
-    if(a < b) {
-        return -1;
-    }
-    return 0;
+let answer = fetch('http://127.0.0.1:5500/').then(response => {
+    return response.text();
+}).then(text => {
+    console.log(text);
 });
-console.log(sorted);
 
-let sorted2 = nums.sort((a,b) => a-b);
-console.log(sorted2);
+console.log(answer);
 
-let squares = nums.map(num => num*num);
-console.log(squares);
+async function getText(){
+let response = await fetch('http://127.0.0.1:5500/');
+console.log(response);
+let text = await response.text();
+console.log(text); 
+}
+
+await getText();
