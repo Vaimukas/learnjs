aa86fa8f7f52909cffd7ceb1a1156d419ca9f677
Greg Ojasalu <greg.suur@gmail.com>
2025-11-04 09:04:48 +0200

    common.js ja module.js, import command

---
+++ common.js
@@ -0,0 +1,5 @@
+module.exports = {
+    hello() {
+        console.log('tere common.js')
+    }
+}
--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-console.log(window, {document});
-let heading = document.querySelector('h1');
-console.log({heading});
-heading.innerText = "gigigogo";
+console.log('Tere terminal!');
 
-function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// const common = require('./common.js');
+import common from './common.js';
 
-let button = document.querySelector('button');
-button.addEventListener('click', event => {
-    console.log(event);
-    heading.innerText = "gogogigi";
-    heading.style.color = getRandomColor();
-    document.documentElement.style.background = getRandomColor();
-    document.body.style.background = getRandomColor();
-});
+common.hello();
 
-setInterval(() => {
-        document.documentElement.style.background = getRandomColor();
-}, 10_000)
+import es from './module.js';
 
-let input = document.querySelector('input');
-input.addEventListener('input', event => {
-    console.log(event);
-    heading.innerText = input.value.split('').reverse().join('');
-})
+es.hello()
