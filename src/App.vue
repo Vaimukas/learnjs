4369861cd166dd83e907eeec600903fc61c4f44e
Greg Ojasalu <greg.suur@gmail.com>
2025-12-02 09:56:28 +0200

    modalid

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+import modal from './components/Modal.vue';
 
-let message = ref('Hello Vue!');
-let isDanger = ref(false);
-let newItem = ref('Piim');
-let i = 0;
-let items = ref([
-    {id: i++, name: 'Viin', isDone: false},
-    {id: i++, name: 'Ween', isDone: false},
-    {id: i++, name: 'Siim', isDone: false},
-    {id: i++, name: 'Liim', isDone: false},
-]);
 
-function add(){
-    if(newItem.value.trim() !== ''){
-         items.value.push({id: i++, name: newItem.value, isDone: false});
-    }
-    newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-
+let modal1Active = ref(false);
+let modal2Active = ref(false);
 </script>
 
 <template>
-    <div class="container mt-3">
-        <div class="content">
-    <button class="button is-primary" @click="message = 'vajutatud'" > vajuta </button>
-    <h1>{{ message }}</h1>
-    <button class="button" :class="{'is-danger': isDanger, 'is-warning': !isDanger}" 
-    @click= "isDanger = !isDanger">vajuta seda ka</button>
-
-
-    <div class="field has-addons mt-3">
-  <div class="control is-expanded">
-    <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="kirjuta siia">
-  </div>
-  <div class="control">
-    <button class="button is-info" @click="add">
-      nüüd vajuta seda
-    </button>
-</div>
-  </div>
-  <h2> {{ newItem.split('').reverse().join('') }}</h2>
-
-  <h1>All items</h1>
-  <li v-for="item in items">{{ item.name }} 
-    <input type="checkbox" v-model="item.isDone">
-  </li>
-
-  <h1>Done items</h1>
-  <li v-for="item in doneItems">{{ item.name }} 
-    <input type="checkbox" v-model="item.isDone">
-  </li>
-
-</div>
+  <div class="container mt-3">
+    <div class="content">
+      <button class="button is-primary" @click="modal1Active = true">Ava Modal 1</button>
+      <button class="button is-link" @click="modal2Active = true">Ava Modal 2</button>
+      
+      <modal :active="modal1Active" @close="modal1Active=false" url="https://picsum.photos/1280/960?random=1"></modal>
+      <modal :active="modal2Active" @close="modal2Active=false" url="https://picsum.photos/1280/960?random=2"></modal>
     </div>
+  </div>
 </template>
---
+++ src/pages/ToDo.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ItemList from './components/ItemList.vue';
+let message = ref('Hello Vue!');
+let isDanger = ref(false);
+let newItem = ref('Piim');
+let i = 0;
+let items = ref([
+    {id: i++, name: 'Viin', isDone: false},
+    {id: i++, name: 'Ween', isDone: false},
+    {id: i++, name: 'Siim', isDone: false},
+    {id: i++, name: 'Liim', isDone: false},
+]);
+
+function add(){
+    if(newItem.value.trim() !== ''){
+         items.value.push({id: i++, name: newItem.value, isDone: false});
+    }
+    newItem.value = '';
+}
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let todoItems = computed(() => items.value.filter(item => !item.isDone));
+
+</script>
+
+<template>
+    <div class="container mt-3">
+        <div class="content">
+    <button class="button is-primary" @click="message = 'vajutatud'" > vajuta </button>
+    <h1>{{ message }}</h1>
+    <button class="button" :class="{'is-danger': isDanger, 'is-warning': !isDanger}" 
+    @click= "isDanger = !isDanger">vajuta seda ka</button>
+
+
+    <div class="field has-addons mt-3">
+  <div class="control is-expanded">
+    <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="kirjuta siia">
+  </div>
+  <div class="control">
+    <button class="button is-info" @click="add">
+      nüüd vajuta seda
+    </button>
+</div>
+  </div>
+  <h2> {{ newItem.split('').reverse().join('') }}</h2>
+  
+<ItemList :items="items" title="All Items"></ItemList>
+<ItemList :items="todoItems" title="To Do Items"></ItemList>
+<ItemList :items="doneItems" title="Done Items"></ItemList>
+
+</div>
+    </div>
+</template>
